080258f9ee88b3d012f37d1763b6d66d47f7be06
ronfromhp <ronaldsamuel2002@gmail.com>
2023-05-18 18:42:46 +0530

    beeg commit

--- RecommendationLibraries/collabfiltering.py
+++ RecommendationLibraries/collabfiltering.py
@@ -4,21 +4,27 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 class MovieRecommender :
-    def putDataset(self, ratings: pd.DataFrame):
+    def putDataset(self, ratings: pd.DataFrame, userid, titleid, ratingid, highestRating : int):
         self.ratings = ratings
-        
-    def putMoviePrefs(self, listOfMovieRatings):
+        self.userid = userid
+        self.titleid = titleid
+        self.ratingid = ratingid
+        self.highestRating = highestRating
+    def putPrefs(self, listOfMovieRatings):
         self.moviePrefs = listOfMovieRatings
         
     def getRatingsByUserId(self):
-        self.ratingsByUserId =  self.ratings.pivot_table(index=['userId'],columns=['title'],values='rating').dropna(thresh=10, axis='columns').fillna(0,axis=1)
+        # debugging
+        self.ratings = self.ratings.groupby(self.userid).filter(lambda x: len(x) >= 200)
+        print(self.ratings)
+        self.ratingsByUserId =  self.ratings.pivot_table(index=self.userid,columns=self.titleid,values=self.ratingid).dropna(thresh=10, axis='columns').fillna(0,axis=1)
         return self.ratingsByUserId
     
     def getCorrelationMatrix(ratingByUserId : pd.DataFrame):
         return ratingByUserId.corr(method='pearson')
     
     def getSimilarToMovie(self, movie_name,rating):
-        similar_ratings = MovieRecommender.getCorrelationMatrix(self.getRatingsByUserId())[movie_name]*(rating-2.5)
+        similar_ratings = MovieRecommender.getCorrelationMatrix(self.getRatingsByUserId())[movie_name]*(rating-float(self.highestRating)/2)
         similar_ratings = similar_ratings.sort_values(ascending=False)
         return similar_ratings
     
@@ -28,19 +34,34 @@ class MovieRecommender :
         similar_movies = similar_movies.sum('columns').sort_values(ascending=False).head(20)
         return similar_movies
 
-def preprocess():
+def preprocessMovies():
     ratings = pd.read_csv('dataset/ratings.csv')
     movies = pd.read_csv('dataset/movies.csv')
     ratings = pd.merge(movies,ratings) .drop(['genres','timestamp'],axis=1)
     return ratings
 
+def preprocessBooks():
+    ratings = pd.read_csv('books-dataset/Ratings.csv')
+    books = pd.read_csv('books-dataset/Books.csv', low_memory= False)
+    bookratin = pd.merge(books, ratings)[['ISBN', 'Book-Title', 'User-ID', 'Book-Rating']]
+    print(bookratin[bookratin.duplicated(['ISBN', 'User-ID'])])
+    return bookratin
 def test():
-    ratings = preprocess()
+    movieratings = preprocessMovies()
+    bookratings = preprocessBooks()
+    print(bookratings.head(20))
+    print(movieratings.head(20))
     recommender = MovieRecommender()
-    recommender.putDataset(ratings)
-    print(recommender.ratings.head())
-    testprefs = [("Avengers, The (2012)",5),("Alice in Wonderland (2010)",1),("Aliens (1986)",1),("2001: A Space Odyssey (1968)",2)]
-    recommender.putMoviePrefs(testprefs)
+    bookrecom = MovieRecommender()
+    bookrecom.putDataset(bookratings, 'User-ID', 'Book-Title', 'Book-Rating', 10)
+    recommender.putDataset(movieratings, 'userId', 'title', 'rating', 5)
+    newprefs = [(2,4), (3,5), (34, 2)]
+    testprefs = [("Shawshank Redemption, The (1994)",5),("Alice in Wonderland (2010)",1),("Aliens (1986)",1),("2001: A Space Odyssey (1968)",2)]
+    bookprefs = [["It", 8]]
+    bookrecom.putPrefs(bookprefs)
+    recommender.putPrefs(testprefs)
+   
     print(recommender.getSimilarToMovies())
+    print(bookrecom.getSimilarToMovies())
     
 test()
