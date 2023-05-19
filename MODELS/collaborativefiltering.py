60520285535b0b82f2cbe1d09eafe69e6c8b5076
ronfromhp <ronaldsamuel2002@gmail.com>
2023-05-19 09:15:28 +0530

    bruh

--- MODELS/collaborativefiltering.py
+++ MODELS/collaborativefiltering.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from scipy import sparse
 from sklearn.metrics.pairwise import cosine_similarity
+from fuzzywuzzy import fuzz, process
 import os
 
 class CollaborativeRecommender :
@@ -31,7 +32,6 @@ class CollaborativeRecommender :
     def getRatingsByUserId(self):
         # debugging
         self.ratings = self.ratings.groupby(self.userid).filter(lambda x: len(x) >= 500)
-        print(self.ratings)
         self.ratingsByUserId =  self.ratings.pivot_table(index=self.userid,columns=self.titleid,values=self.ratingid).dropna(thresh=10, axis='columns').fillna(0,axis=1)
         return self.ratingsByUserId
     
@@ -54,6 +54,19 @@ class CollaborativeRecommender :
         similar_movies = pd.concat([self.getSimilarToMovie(movie, rating) for movie, rating in self.moviePrefs],  axis=1,ignore_index=True)
         similar_movies = similar_movies.sum('columns').sort_values(ascending=False).head(20)
         return similar_movies
+    def find_closest_matches(self, query):
+        matches = []
+        limit = 0
+        print(self.getRatingsByUserId().columns)
+        for moviez in self.getRatingsByUserId().columns.tolist():
+            # print(query.lower(), moviez.lower())
+            limit += 1
+            if query.lower() in moviez.lower() :
+                matches.append(moviez)
+            if limit > 3500:
+                break
+        return matches       
+   
 
 def preprocessMovies():
     ratings = pd.read_csv('./CSV/movies-dataset/movie_ratings.csv')
@@ -65,14 +78,14 @@ def preprocessBooks():
     ratings = pd.read_csv('./CSV/books-dataset/Ratings.csv')
     books = pd.read_csv('./CSV/books-dataset/Books.csv', low_memory= False)
     bookratin = pd.merge(books, ratings)[['ISBN', 'Book-Title', 'User-ID', 'Book-Rating']]
-    print(bookratin[bookratin.duplicated(['ISBN', 'User-ID'])])
+    # print(bookratin[bookratin.duplicated(['ISBN', 'User-ID'])])
     return bookratin
 def test():
     # # make sure to preprocess the movie and book datasets
     movieratings = preprocessMovies()
     bookratings = preprocessBooks()
-    print(bookratings.head(20))
-    print(movieratings.head(20))
+    # print(bookratings.head(20))
+    # print(movieratings.head(20))
     recommender = CollaborativeRecommender(movieratings, 'userId', 'title', 'rating', 5)
     bookrecom = CollaborativeRecommender(bookratings, 'User-ID', 'Book-Title', 'Book-Rating', 10)
     recommender.loadCorrelationMatrix()
@@ -80,10 +93,12 @@ def test():
     bookprefs = [["It", 8]]
     bookrecom.putPrefs(bookprefs)
     recommender.putPrefs(testprefs)
+    toitles = recommender.getRatingsByUserId().columns.tolist()
+    # print(toitles)
     
-    print(recommender.getSimilarToMovies())
-    print(bookrecom.getSimilarToMovies())
+    # print(bookrecom.getSimilarToMovies())
+    # print(fuzz.ratio("shawshank redemption", "Red"))
     # save the trained correlation matrix
     recommender.saveCorrelationMatrix()
+    print(bookrecom.find_closest_matches("pr"))
     
-test()
